7915444f13e8a5c94d2ac3b51791f721b00989f9
rupesh <rupeshnarkar500@gmail.com>
2020-07-21 14:58:57 +0530

    fixing main navbar code

---
+++ main-files/navbar.php
@@ -0,0 +1,157 @@
+            <header>
+                <div class="container no-padding">
+                    <div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
+                        <!-- ============================================================= LOGO ============================================================= -->
+                        <div class="logo">
+                            <a href="index.html">
+                            
+                            
+<!--                           WEBSITE LOGO HERE-->
+                               <img src="" alt="logo" height="54px" width="223px">
+                      
+                                
+                            </a>
+                        </div><!-- /.logo -->
+                        <!-- ============================================================= LOGO : END ============================================================= -->
+                    </div><!-- /.logo-holder -->
+
+                    <div class="col-xs-12 col-sm-12 col-md-6 top-search-holder no-margin">
+                        <div class="contact-row">
+                            <div class="phone inline">
+                                <i class="fa fa-phone"></i> (+800) 123 456 7890
+                            </div>
+                            <div class="contact inline">
+                                <i class="fa fa-envelope"></i> contact@<span class="le-color">oursupport.com</span>
+                            </div>
+                        </div><!-- /.contact-row -->
+                        <!-- ============================================================= SEARCH AREA ============================================================= -->
+                        <div class="search-area">
+                            <form>
+                                <div class="control-group">
+                                    <input class="search-field" placeholder="Search for item" />
+
+                                    <ul class="categories-filter animate-dropdown">
+                                        <li class="dropdown">
+
+                                            <a class="dropdown-toggle"  data-toggle="dropdown" href="category-grid.html">all categories</a>
+
+                                            <ul class="dropdown-menu" role="menu" >
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" href="category-grid.html">laptops</a></li>
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" href="category-grid.html">tv & audio</a></li>
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" href="category-grid.html">gadgets</a></li>
+                                                <li role="presentation"><a role="menuitem" tabindex="-1" href="category-grid.html">cameras</a></li>
+
+                                            </ul>
+                                        </li>
+                                    </ul>
+
+                                    <a class="search-button" href="#" ></a>
+
+                                </div>
+                            </form>
+                        </div><!-- /.search-area -->
+                        <!-- ============================================================= SEARCH AREA : END ============================================================= -->
+                    </div><!-- /.top-search-holder -->
+
+                    <div class="col-xs-12 col-sm-12 col-md-3 top-cart-row no-margin">
+                        <div class="top-cart-row-container">
+                            <div class="wishlist-compare-holder">
+                                <div class="wishlist ">
+                                    <a href="#"><i class="fa fa-heart"></i> wishlist <span class="value">(21)</span> </a>
+                                </div>
+                                <div class="compare">
+                                    <a href="#"><i class="fa fa-exchange"></i> compare <span class="value">(2)</span> </a>
+                                </div>
+                            </div>
+
+                            <!-- ============================================================= SHOPPING CART DROPDOWN ============================================================= -->
+                            <div class="top-cart-holder dropdown animate-dropdown">
+                                <div class="basket">
+                                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                        <div class="basket-item-count">
+                                            <span class="count">3</span>
+                                            <img src="assets/images/icon-cart.png" alt="" />
+                                        </div>
+
+                                        <div class="total-price-basket">
+                                            <span class="lbl">your cart:</span>
+                                            <span class="total-price">
+                                                <span class="sign">$</span><span class="value">3219,00</span>
+                                            </span>
+                                        </div>
+                                    </a>
+
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <div class="basket-item">
+                                                <div class="row">
+                                                    <div class="col-xs-4 col-sm-4 no-margin text-center">
+                                                        <div class="thumb">
+                                                            <img alt="" src="assets/images/products/product-small-01.jpg" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xs-8 col-sm-8 no-margin">
+                                                        <div class="title">Blueberry</div>
+                                                        <div class="price">$270.00</div>
+                                                    </div>
+                                                </div>
+                                                <a class="close-btn" href="#"></a>
+                                            </div>
+                                        </li>
+
+                                        <li>
+                                            <div class="basket-item">
+                                                <div class="row">
+                                                    <div class="col-xs-4 col-sm-4 no-margin text-center">
+                                                        <div class="thumb">
+                                                            <img alt="" src="assets/images/products/product-small-01.jpg" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xs-8 col-sm-8 no-margin">
+                                                        <div class="title">Blueberry</div>
+                                                        <div class="price">$270.00</div>
+                                                    </div>
+                                                </div>
+                                                <a class="close-btn" href="#"></a>
+                                            </div>
+                                        </li>
+
+                                        <li>
+                                            <div class="basket-item">
+                                                <div class="row">
+                                                    <div class="col-xs-4 col-sm-4 no-margin text-center">
+                                                        <div class="thumb">
+                                                            <img alt="" src="assets/images/products/product-small-01.jpg" />
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-xs-8 col-sm-8 no-margin">
+                                                        <div class="title">Blueberry</div>
+                                                        <div class="price">$270.00</div>
+                                                    </div>
+                                                </div>
+                                                <a class="close-btn" href="#"></a>
+                                            </div>
+                                        </li>
+
+
+                                        <li class="checkout">
+                                            <div class="basket-item">
+                                                <div class="row">
+                                                    <div class="col-xs-12 col-sm-6">
+                                                        <a href="cart.html" class="le-button inverse">View cart</a>
+                                                    </div>
+                                                    <div class="col-xs-12 col-sm-6">
+                                                        <a href="checkout.html" class="le-button">Checkout</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+
+                                    </ul>
+                                </div><!-- /.basket -->
+                            </div><!-- /.top-cart-holder -->
+                        </div><!-- /.top-cart-row-container -->
+                        <!-- ============================================================= SHOPPING CART DROPDOWN : END ============================================================= -->
+                    </div><!-- /.top-cart-row -->
+                </div><!-- /.container -->
+            </header>
